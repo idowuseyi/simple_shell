601aea868cb1b7fd797138a7379bb9556ffc27a5
idowuseyi <idowuseyi22@gmail.com>
2022-09-02 13:57:14 +0100

    testing

--- test2/parser.c
+++ test2/parser.c
@@ -7,6 +7,26 @@ void parser(char *buf, char *buf_cpy)
 	char delim[] = " \n";
 	char **argv = NULL;
 
-	
+	/* specify how many token are present in buf
+	* this helps determine size of argv array
+	*/
+
+	token = strtok(buf, delim);
+	while (token)
+	{
+		num_tok++;
+		token = strtok(NULL, delim);
+	}
+
+	num_tok++;
+	argv = malloc(sizeof(char *) * num_tok);
+
+	token = strtok(buf_cpy, delim);
+	for (i = 0; token; i++)
+	{
+		argv[i] = malloc(sizeof(char) * strlen(token));
+		strcpy(argv[i], token);
+		token = strtok(NULL, delim);
+	}
 
 }
