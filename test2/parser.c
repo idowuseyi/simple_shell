c29a38500e686dd071bc707142c9acd618713107
idowuseyi <idowuseyi22@gmail.com>
2022-09-02 13:50:00 +0100

    testing

---
+++ test2/parser.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+void parser(char *buf, char *buf_cpy)
+{
+	char *token;
+	int num_tok = 0, i;
+	char delim[] = " \n";
+	char **argv = NULL;
+
+	
+
+}
---
+++ test2/shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_cmd(void);
+void parser(char *buf, char *buf_cpy);
+
+
+
+#endif
